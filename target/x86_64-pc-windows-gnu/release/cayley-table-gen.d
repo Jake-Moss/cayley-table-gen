ee63712a443832b82984c8a876199cf35fb75e31
Jake Moss <jake.moss@uqconnect.edu.au>
2021-02-25 13:31:58 +1000

    Windows compilation

--- target/x86_64-pc-windows-gnu/release/cayley-table-gen.d
+++ target/x86_64-pc-windows-gnu/release/cayley-table-gen.d
@@ -1,1 +1,1 @@
-/home/jake/Projects/Cayley-table-gen/target/x86_64-pc-windows-gnu/release/cayley-table-gen.exe: /home/jake/Projects/Cayley-table-gen/src/main.rs
+/home/jake/Side\ Projects/cayley-table-gen/target/x86_64-pc-windows-gnu/release/cayley-table-gen.exe: /home/jake/Side\ Projects/cayley-table-gen/src/main.rs
